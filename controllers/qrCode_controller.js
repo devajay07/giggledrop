cee1ce0ca847d12150e51dba2223789e5c506f4d
Ajay Yadav <ayyadav179@gmail.com>
2023-02-26 03:34:30 +0530

    completed the devlopment

--- controllers/qrCode_controller.js
+++ controllers/qrCode_controller.js
@@ -1,8 +1,7 @@
 const qrcode = require('qrcode');
 
 exports.generateQr = async (req, res, next) =>{
-    const downloadLink = `${process.env.APP_BASE_URL}/files/download/${req.query.fileuuid}`;
-    const qrCode = await qrcode.toDataURL(downloadLink);
+    const qrCode = await qrcode.toDataURL(req.query.link);
     res.send(`<img src="${qrCode}">`);
 
 }
